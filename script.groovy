a8a886230064e7b911e60bd4e3c4455ecbb8bef9
poornambiga <83727595+poornambiga@users.noreply.github.com>
2022-01-07 14:34:30 +0530

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,14 @@
+def buildJar() {
+    echo "building the application..."
+} 
+
+def buildImage() {
+    echo "building the docker image..."
+     }
+} 
+
+def deployApp() {
+    echo 'deploying the application...'
+} 
+
+return this
